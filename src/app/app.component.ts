6eb85a7ceea13d98b5c076f7f164288d54c57766
Bort190 <heiko.nevoigt@googlemail.com>
2024-05-15 20:04:43 +0200

    components complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { StartscreenComponent } from './startscreen/startscreen.component';
 import { DescriptionComponent } from './description/description.component';
 import { SkillsetComponent } from './skillset/skillset.component';
 import { MyworkComponent } from './mywork/mywork.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StartscreenComponent, DescriptionComponent, SkillsetComponent, MyworkComponent],
+  imports: [CommonModule, RouterOutlet, StartscreenComponent, DescriptionComponent, SkillsetComponent, MyworkComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -2,13 +2,14 @@ import { Component, Inject, inject } from '@angular/core';
 import { ProjectComponent } from './project/project.component';
 import { ProjectsService } from '../projects.service';
 import { CommonModule } from '@angular/common';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @Component({
   selector: 'app-mywork',
   standalone: true,
-  imports: [ProjectComponent, CommonModule],
+  imports: [ProjectComponent, CommonModule, ContactComponent],
   templateUrl: './mywork.component.html',
   styleUrl: './mywork.component.scss'
 })
