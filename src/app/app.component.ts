d0874ffbef990b556f3bc57589d7ec7df969f540
Bort190 <heiko.nevoigt@googlemail.com>
2024-05-28 17:55:07 +0200

    translate2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
@@ -6,15 +6,29 @@ import { DescriptionComponent } from './description/description.component';
 import { SkillsetComponent } from './skillset/skillset.component';
 import { MyworkComponent } from './mywork/mywork.component';
 import { FooterComponent } from './footer/footer.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StartscreenComponent, DescriptionComponent, SkillsetComponent, MyworkComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, StartscreenComponent, DescriptionComponent, SkillsetComponent, MyworkComponent, FooterComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  languages = ['de', 'en'];
+  private translateService = inject(TranslateService);
+  
+  ngOnInit(): void {
+    const defaultLanguage = localStorage.getItem('language') || 'de';
+    this.translateService.setDefaultLang(defaultLanguage);
+    this.translateService.use(defaultLanguage);
+  }
+
+  changeLanguage(lang: string){
+    this.translateService.use(lang);
+    localStorage.setItem('language' ,lang)
+  }
   title = 'portfolio';
 }
--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-startscreen',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TranslateModule],
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
 })
