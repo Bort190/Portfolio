acc915a9148e11a36ae26bc101063f56c94e4240
Bort190 <heiko.nevoigt@googlemail.com>
2024-05-29 20:24:54 +0200

    translate4

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-description',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './description.component.html',
   styleUrl: './description.component.scss'
 })
