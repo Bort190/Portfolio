45dbe59a6a60ff2721a1b1cbf43005d1bf36ee56
Bort190 <heiko.nevoigt@googlemail.com>
2024-06-16 13:03:17 +0200

    second finish

--- src/app/mywork/project/project.component.ts
+++ src/app/mywork/project/project.component.ts
@@ -15,19 +15,22 @@ export class ProjectComponent {
     AOS.init();
   }
 
+
+  
   @Input() project = {
-    img: 'join.png',
-    name: 'Join',
+    img: 'tombstone.png',
+    name: 'Tombstone Tales',
     skills: 'Javascript | CSS | HTML',
-    liveLink: '',
-    githubLink: '',
+    liveLink: 'http://heiko-nevoigt.developerakademie.net/Tombstone%20Tales/startscreen.html',
+    githubLink: 'https://github.com/Bort190/Tombstone-Tales',
     description:
-      'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-    descriptionEn: 'description in english',
+      'Ein Jump-and-run Spiel, welches mithilfe eines objektorientierten Ansatzes in JavaScript umgesetzt wurde. Stelle dich als tapferer Sensenmann Zombiehorden entgegen. Du hast die Wahl, sie entweder mit deinem Kampfstab anzugreifen oder ihnen Knochen entgegen zu werfen, bis du dich am Ende deiner Reise einer noch größeren Gefahr stellen musst.',
+    descriptionEn: 'A jump-and-run game implemented using an object-oriented approach in JavaScript. Face hordes of zombies as the brave Grim Reaper. You have the choice to attack them with your staff or throw bones at them until you must confront an even greater danger at the end of your journey.',
   };
+
   descriptionTranslate = {
     description: this.project.description,
-    descriptionEn: this.project.descriptionEn,
+    descriptionEn: this.project.descriptionEn
   };
 
   @Input() index = 0;
--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -14,8 +14,8 @@ export class ProjectsService {
       liveLink: 'http://heiko-nevoigt.developerakademie.net/Join_Gruppenarbeit/index.html',
       githubLink: 'https://github.com/maksmuehlbauer/Join_Gruppenarbeit',
       description:
-        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      descriptionEn: 'description in english',
+        'Ein Task-Management Programm in Anlehnung an ein Kanban Board. Es erlaubt dem Nutzer Aufgaben zu erstellen und Kontakte zuzuordnen und diese dann per drag and drop in verschiedene Kategorien zu organisieren.',
+      descriptionEn: 'A task management program inspired by a Kanban board. It allows the user to create tasks and assign contacts, then organize them into different categories using drag and drop.',
     },
     {
       img: 'tombstone.png',
@@ -24,8 +24,8 @@ export class ProjectsService {
       liveLink: 'http://heiko-nevoigt.developerakademie.net/Tombstone%20Tales/startscreen.html',
       githubLink: 'https://github.com/Bort190/Tombstone-Tales',
       description:
-        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
-      descriptionEn: 'description in english2',
+        'Ein Jump-and-run Spiel, welches mithilfe eines objektorientierten Ansatzes in JavaScript umgesetzt wurde. Stelle dich als tapferer Sensenmann Zombiehorden entgegen. Du hast die Wahl, sie entweder mit deinem Kampfstab anzugreifen oder ihnen Knochen entgegen zu werfen, bis du dich am Ende deiner Reise einer noch größeren Gefahr stellen musst.',
+      descriptionEn: 'A jump-and-run game implemented using an object-oriented approach in JavaScript. Face hordes of zombies as the brave Grim Reaper. You have the choice to attack them with your staff or throw bones at them until you must confront an even greater danger at the end of your journey.',
     },
     {
       img: 'crm.png',
@@ -34,8 +34,8 @@ export class ProjectsService {
       liveLink: '',
       githubLink: '',
       description:
-        'A very Simple Customer Relationship Management system working with CRUD functionality.',
-      descriptionEn: 'description in english3',
+        'Ein sehr einfaches Customer-Relationship-Management-System, das mit CRUD-Funktionalität arbeitet.',
+      descriptionEn: 'A very Simple Customer Relationship Management system working with CRUD functionality.',
     },
   ];
 }
