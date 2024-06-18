e03a2f94c3c6596e69a26b353b003a6da7b0eed7
Bort190 <heiko.nevoigt@googlemail.com>
2024-06-18 18:01:27 +0200

    fourth finish

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
@@ -8,4 +9,14 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
-export class ImprintComponent {}
+export class ImprintComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(function () {
+          window.scrollTo(0, 0);
+        }, 1);
+      }
+    });
+  }
+}
