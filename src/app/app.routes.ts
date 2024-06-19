5d6a9dfdbfffe7780779f6b3dc9ec84b1e1feb21
Heiko Nevoigt <143389650+Bort190@users.noreply.github.com>
2024-06-19 09:24:13 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MainContentComponent } from './main-content/main-content.component';
-
+import { SkillsetComponent } from './skillset/skillset.component';
+import { MyworkComponent } from './mywork/mywork.component';
+import { ContactComponent } from './mywork/contact/contact.component';
+import { DescriptionComponent } from './description/description.component';
 
 export const routes: Routes = [
-    {path: '', component: MainContentComponent},
-    {path: 'imprint', component: ImprintComponent},
-    {path: 'privacy', component: ImprintComponent},
+  {
+    path: '',
+    component: MainContentComponent,
+    children: [
+      { path: 'about-me', component: DescriptionComponent },
+      { path: 'my-skills', component: SkillsetComponent },
+      { path: 'my-work', component: MyworkComponent },
+      { path: 'contact', component: ContactComponent },
+    ],
+  },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy', component: ImprintComponent },
 ];
